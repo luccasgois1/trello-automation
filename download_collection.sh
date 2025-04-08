2af1d7bcd6b3e37ec0c483717d1c1b1ce5c47968
luccasgois <luccasgois1@gmail.com>
2025-04-08 08:01:12 -0300

    Adding postman_collection and download_script

---
+++ download_collection.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+set -ex
+
+SCRIPT_DIR=$(dirname $(readlink -f "$0"))
+
+POSTMAN_COLLECTION_FILE="$SCRIPT_DIR/postman_collection.json"
+ENV_FILE="$SCRIPT_DIR/.env"
+
+if [ ! -f "$ENV_FILE" ]; then
+    echo "$ENV_FILE file does not exist. Please create one based on the README.md file" 
+    exit 1
+fi
+
+source $ENV_FILE
+
+COLLECTION_UID=22855432-4ddc7e7b-fdc1-4f82-bee1-b738a657f547
+
+curl --location --request GET "https://api.getpostman.com/collections/$COLLECTION_UID" \
+--header "X-API-Key: $POSTMAN_KEY" > "$POSTMAN_COLLECTION_FILE"
